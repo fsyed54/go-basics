4986d109751ec7b516dd83304007189cd310e8d3
Syed Wassay Uddin <fsyed@miraclesoft.com>
2024-04-25 11:31:33 -0400

    Interfaces and Custom Writer

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	
-	io.Copy(os.Stdout, resp.Body) //os.Stdout is write interface
+	lw := logWriter{}
+	io.Copy(lw, resp.Body) //os.Stdout/lw is writer interface // resp.Body is reader interface
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
