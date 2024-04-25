c0f490db042327557d198c018fef2ce8eb2fe170
Syed Wassay Uddin <fsyed@miraclesoft.com>
2024-04-24 21:23:27 -0400

    Manipulating Maps

--- map/main.go
+++ map/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map [string] string{
-		"red": "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string] string
+
+	colors := make(map[string] string)
+
+	// colors := map [string] string{ // key value 
+	// 	"red": "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors["White"] = "#fffff"
+
+	delete(colors, "White")
+
 	fmt.Println(colors)
 }
